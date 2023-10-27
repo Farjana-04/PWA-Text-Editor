18254b29b02565b8d3ba428278fed9622c374a44
Farjana Akhter <farjana.akhter085@gmail.com>
2023-10-27 14:45:31 -0500

    coded for Add and configure workbox plugins for a service worker and manifest file in client/webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,7 +6,6 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 const WorkboxPlugin = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => {
   return {
@@ -24,7 +23,6 @@ module.exports = () => {
         template: "./index.html",
         title: "Webpack Plugin",
       }),
-      // new MiniCssExtractPlugin(),
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
