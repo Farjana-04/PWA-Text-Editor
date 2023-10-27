d3ed3c55ac6132e8d79512c81bea51475381ab7f
Farjana Akhter <farjana.akhter085@gmail.com>
2023-10-27 15:21:54 -0500

    Exported a function we will use to GET to the database in client/src/js/database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -31,7 +31,6 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  //console.error('getDb not implemented');
   console.log('GET single from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
