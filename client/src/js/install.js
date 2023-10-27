9cbd3635f2cb89c92bbda8b84e855d5fd4e72921
Farjana Akhter <farjana.akhter085@gmail.com>
2023-10-27 15:04:28 -0500

    Implemented a click event handler on the  element, show the prompt in client/src/js/install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,11 +2,8 @@
 
 
 // // TODO: Implement a click event handler on the `butInstall` element
-// butInstall.addEventListener('click', async () => {});
-
 // // TODO: Add an handler for the `appinstalled` event
-// window.addEventListener('appinstalled', (event) => {});
-// const butInstall = document.getElementById('buttonInstall');
+
 
 // // Logic for installing the PWA
 // // TODO: Add an event handler to the `beforeinstallprompt` event
