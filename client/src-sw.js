c7abbd2a9a2cf3ad37bc3ec565bd9cb0634c9381
Farjana Akhter <farjana.akhter085@gmail.com>
2023-10-27 14:48:45 -0500

    Implemented asset caching in client/src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -23,7 +23,6 @@ warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
-
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
